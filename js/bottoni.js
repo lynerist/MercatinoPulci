03f4b680c0ef373ad37c2064dc430e412944e865
lynerist <leonardo.albani@studenti.unimi.it>
2020-12-08 21:22:38 +0100

    bottoni

--- js/bottoni.js
+++ js/bottoni.js
@@ -1,7 +1,14 @@
 function rimuovi(me, other) {
-	var m = document.getElementById(me);
-	var o = document.getElementById(other);
+	let m = document.getElementById(me);
+	let o = document.getElementById(other);
+	let box = document.getElementById(m.id.substring(7));
 
 	m.style.display = "none";
 	o.style.display = "block";
+	if (box.style.opacity === "0.5"){
+		box.style.opacity = "1";
+	}else{
+		box.style.opacity = "0.5";
+		o.style.opacity = "1";
+	}
 }

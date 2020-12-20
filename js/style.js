03a9898629dc29de4e84fe3f5def6d5c475f705d
FilippoUslenghi <us98@hotmail.it>
2020-12-20 01:32:58 +0100

    index e profilo finiti

---
+++ js/style.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+    $('#openBtn').click(function () {
+        $('#modalModificaAnnuncio').modal({
+            show: true
+        })
+    });
+
+    $(document).on('show.bs.modal', '.modal', function (event) {
+        var zIndex = 1040 + (10 * $('.modal:visible').length);
+        $(this).css('z-index', zIndex);
+        setTimeout(function() {
+            $('.modal-backdrop').not('.modal-stack').css('z-index', zIndex - 1).addClass('modal-stack');
+        }, 0);
+    });
+
+});

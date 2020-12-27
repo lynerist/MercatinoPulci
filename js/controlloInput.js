881edfd58daaf211560ee54b1610143649fc30c7
lynerist <leonardo.albani@studenti.unimi.it>
2020-12-27 22:02:38 +0100

    mando

---
+++ js/controlloInput.js
@@ -0,0 +1,24 @@
+function apriNuovoAnnuncio(){
+	window.open("/annuncio.html");
+}
+
+(function() {
+	'use strict';
+	window.addEventListener('load', function() {
+	// Fetch all the forms we want to apply custom Bootstrap validation styles to
+	var forms = document.getElementsByClassName('needs-validation');
+
+	// Loop over them and prevent submission
+	var validation = Array.prototype.filter.call(forms, function(form) {
+		form.addEventListener('submit', function(event) {
+		if (form.checkValidity() === false) {
+			event.preventDefault();
+			event.stopPropagation();
+		}else if (form.id === "nuovoAnnuncio") {
+			apriNuovoAnnuncio()
+		}
+		form.classList.add('was-validated');
+		}, false);
+	});
+	}, false);
+})();

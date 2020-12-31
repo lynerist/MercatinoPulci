f91c47fe7fdb92abb3260e8d8fda2abfea754d3f
lynerist <leonardo.albani@studenti.unimi.it>
2020-12-31 01:00:56 +0100

    controlli

--- js/controlloInput.js
+++ js/controlloInput.js
@@ -2,33 +2,41 @@ function apriNuovoAnnuncio() {
     window.open("/annuncio.html");
 }
 
-function prova(id) {
-    let myform = $('#' + id);
-    myform.addEventListener()
+function controllaSubmit(id) {
+    let myform = document.getElementById(id);
+
     myform.addEventListener('submit', function (event) {
-        event.preventDefault();
-        event.stopPropagation();
+        if (!controllaForm(id)){
+            event.preventDefault();
+            event.stopPropagation();
+        }
     }, false);
 }
 
+function controllaForm(id){
+    for (let input of document.getElementById(id).children){
+        if (input.classList.contains("is-invalid")){
+            return false
+        }
+    }
+    return true
+}
 
-(function () {
-    'use strict';
-    window.addEventListener('load', function () {
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        var forms = document.getElementsByClassName('needs-validation');
+function colora(id, bool){
+    let element = document.getElementById(id);
+    if (bool) {
+        element.classList.add("is-valid");
+        element.classList.remove("is-invalid");
+    }else{
+        element.classList.add("is-invalid");
+        element.classList.remove("is-valid");
+    }
+}
 
-        // Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, function (form) {
-            form.addEventListener('submit', function (event) {
-                if (!form.checkValidity()) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                } else if (form.id === "nuovoAnnuncio") {
-                    apriNuovoAnnuncio()
-                }
-                form.classList.add('was-validated');
-            }, false);
-        });
-    }, false);
-})();
+function controllaCodiceFiscale(codice){
+    return /^(?:[A-Z][AEIOU][AEIOUX]|[B-DF-HJ-NP-TV-Z]{2}[A-Z]){2}(?:[\dLMNP-V]{2}(?:[A-EHLMPR-T](?:[04LQ][1-9MNP-V]|[15MR][\dLMNP-V]|[26NS][0-8LMNP-U])|[DHPS][37PT][0L]|[ACELMRT][37PT][01LM]|[AC-EHLMPR-T][26NS][9V])|(?:[02468LNQSU][048LQU]|[13579MPRTV][26NS])B[26NS][9V])(?:[A-MZ][1-9MNP-V][\dLMNP-V]{2}|[A-M][0L](?:[1-9MNP-V][\dLMNP-V]|[0L][1-9MNP-V]))[A-Z]$/.test(codice.toUpperCase());
+}
+
+function controllaEmail(email){
+    return /\S+@\S+.\S+/.test(email);
+}
